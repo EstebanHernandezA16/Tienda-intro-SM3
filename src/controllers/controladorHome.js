e1dd7c113ea6c0360acf20c9bed8670b8abb5487
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-02-22 08:48:04 -0500

    Se crean datos de producto

--- src/controllers/controladorHome.js
+++ src/controllers/controladorHome.js
@@ -1,8 +1,31 @@
 // definiendo datos var para global, let para local
+import{productos} from '../helpers/baseDatos.js'
 
-let nombre="James Rodriguez"
-if(5>2){
-let nombre="Lucho Diaz"
-}
+//llamando a la fila
+let fila=document.getElementById("fila")
 
-console.log("hola "+nombre)
+//usando foreach
+productos.forEach(function(producto){
+
+    //creando columnas para alojar los productos
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+    
+
+    //crear tarjeta para almacenar la info del producto
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100","shadow")
+
+    //crear una imagen
+    let imagen=document.createElement("img")
+    imagen.classList.add("img.fluid","w-100")
+
+    //herencia del mas interno al mas externo
+
+    tarjeta.appendChild(imagen)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+    imagen.src=producto.fotos[0]
+
+})
---
+++ src/helpers/baseDatos.js
@@ -0,0 +1,40 @@
+//quemar/setear una base de datos
+//el nombre de todo objeto va en plural
+
+export let productos=Array(
+    {
+        nombre:"Producto1",
+        descripcion:"Descripcion aqui",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/eha-tienda.appspot.com/o/consola1E.jpg?alt=media&token=b3ec36a7-a024-42d5-a9a4-5dfa7f8339f4","na2"),
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto2",
+        descripcion:"Descripcion aqui",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/eha-tienda.appspot.com/o/consola2E.jpg?alt=media&token=a60fdd4a-72e5-4619-83ec-58389a9289fe","na2"),
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto3",
+        descripcion:"Descripcion aqui",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/eha-tienda.appspot.com/o/Consola3E.jpg?alt=media&token=7404fe21-e5d5-45c3-8602-77396e1006e3","na2"),
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto4",
+        descripcion:"Descripcion aqui",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/eha-tienda.appspot.com/o/consola4SE.jpg?alt=media&token=b0c247c8-4971-4d4c-980a-8d614947966e","na2"),
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto5",
+    descripcion:"Descripcion aqui",
+    fotos:Array("https://firebasestorage.googleapis.com/v0/b/eha-tienda.appspot.com/o/consola5E.jpg?alt=media&token=7ce9ea95-773e-4c79-b18f-0cd6eade2b5f","na2"),
+    precio:70000,
+    popularidad:4.2
+}
+)
